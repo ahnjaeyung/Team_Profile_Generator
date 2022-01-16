745b0b3d20ae4853c920f4926567365d39613f81
ahnjaeyung <ahn.jaeyung@gmail.com>
2022-01-16 16:59:33 -0600

    added manager subclass

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,4 +1,15 @@
 const Employee = require('./Employee');
 class Manager extends Employee {
-    constructor(officeNumber, )
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber
+    }
+    getRole() {
+        return "Manager";
+    } //end of getRole() function definition
+    getOfficeNumber() {
+        return this.officeNumber
+    }
 }
+
+module.exports = Manager;
